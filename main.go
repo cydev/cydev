deb36556629e2b383548bac5540127c89d3b44cd
ernado <ernado@ya.ru>
2016-03-02 03:03:16 +0300

    moving to fasthttp

--- main.go
+++ main.go
@@ -1,46 +1,75 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"log"
-	"net/http"
 
-	"github.com/bradfitz/http2"
-	"github.com/labstack/echo"
-	mw "github.com/labstack/echo/middleware"
+	"github.com/valyala/fasthttp"
 )
 
 const (
-	version = "1a"
+	version = "1.1a"
 )
 
 var (
-	addr = flag.String("addr", "localhost:1234", "Addr to listen")
-	cert = flag.String("cert", "", "TLS cert")
-	key  = flag.String("key", "", "TLS key")
+	addr    = flag.String("addr", "localhost:1234", "Addr to listen")
+	addrTLS = flag.String("addrTLS", "", "Addr to listen TLS")
+	cert    = flag.String("cert", "", "TLS cert")
+	key     = flag.String("key", "", "TLS key")
 )
 
-func static(c *echo.Context) error {
-	return nil
+func NewPathStripper() fasthttp.PathRewriteFunc {
+	var staticPrefix = []byte("/static")
+	return func(ctx *fasthttp.RequestCtx) []byte {
+		path := ctx.Path()
+		if bytes.HasPrefix(path, staticPrefix) {
+			path = path[len(staticPrefix):]
+		}
+		return path
+	}
 }
 
 func main() {
 	flag.Parse()
-	e := echo.New()
-	e.Use(mw.Recover())
-	e.Use(mw.Logger())
-	e.Index("static/index.html")
-	e.ServeFile("/hath", "static/hath.html")
-	e.Static("/static", "static")
 
-	var srv http.Server
-	srv.Addr = *addr
-	srv.Handler = e
-	log.Println("cydev.ru listening on", *addr, "version", version)
-	http2.ConfigureServer(&srv, nil)
-	if len(*key) != 0 && len(*cert) != 0 {
-		log.Fatal(srv.ListenAndServeTLS(*cert, *key))
-	} else {
-		log.Fatal(srv.ListenAndServe())
+	fs := &fasthttp.FS{
+		Root:               "static",
+		IndexNames:         []string{"index.html"},
+		GenerateIndexPages: false,
+		Compress:           true,
+		AcceptByteRange:    true,
+		PathRewrite:        NewPathStripper(),
 	}
+
+	fsHandler := fs.NewRequestHandler()
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		switch {
+		case bytes.HasPrefix(ctx.Path(), []byte("/hath")):
+			fasthttp.ServeFile(ctx, "static/hath.html")
+		default:
+			fsHandler(ctx)
+		}
+	}
+
+	// Start HTTP server.
+	if len(*addr) > 0 {
+		log.Printf("Starting HTTP server on %q", *addr)
+		go func() {
+			if err := fasthttp.ListenAndServe(*addr, requestHandler); err != nil {
+				log.Fatalf("error in ListenAndServe: %s", err)
+			}
+		}()
+	}
+
+	// Start HTTPS server.
+	if len(*addrTLS) > 0 {
+		log.Printf("Starting HTTPS server on %q", *addrTLS)
+		go func() {
+			if err := fasthttp.ListenAndServeTLS(*addrTLS, *cert, *key, requestHandler); err != nil {
+				log.Fatalf("error in ListenAndServeTLS: %s", err)
+			}
+		}()
+	}
+	select {}
 }
