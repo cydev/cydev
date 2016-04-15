3e227c9658455ceda1a891d9b51f6e59c195e2d9
Aleksandr Razumov <ar@cydev.ru>
2016-04-16 02:21:52 +0300

    refactor set 2; update stok

--- fabfile.py
+++ fabfile.py
@@ -6,26 +6,12 @@ import logging
 logging.basicConfig()
 
 env.user = 'cydev'
-env.base_dir = '/home/cydev'
+env.base_dir = '/cydev'
 env.use_ssh_config = True
 env.hosts = ['cydev.ru']
 
 
 def deploy(branch='master', restart='yes'):
-    local('go build')
     with cd(env.base_dir):
-        run('rm cydev.ru')
-        put('cydev', 'cydev.ru')
-        run('chmod +x cydev.ru')
-        run('git fetch')
-        run('git checkout %s' % branch)
-        run('git pull --rebase origin %s' % branch)
-
-def restart():
-    env.user = 'root'
-    run('systemctl restart cydev')
-
-
-def status():
-    env.user = 'root'
-    run('systemctl status cydev')
+        run('git fetch --all')
+        run('git reset --hard origin/master')
